eead75f671582cacb9d5b4493204df238027b308
aian <k0545421@kadai.jp>
2026-04-01 19:05:57 +0900

    configureを追記

--- run/run.sh
+++ run/run.sh
@@ -12,3 +12,26 @@ cd $OUTDIR
 # 実行（ファイル名はinputに任せる）
 # 環境作成中のテスト計算用入力ファイルはToyouchi_testディレクトリに格納
 ../../bin/athena -i ../../inputs/hydro/Toyouchi_test/athinput.Toyouchi
+
+# configure
+  #Problem generator:          jeans
+  #Coordinate system:          cartesian
+  #Equation of state:          isothermal
+  #Riemann solver:             hlle
+  #Magnetic fields:            OFF
+  #Relativistic dynamics:      OFF 
+  #General relativity:         OFF 
+  #Radiative Transfer:         OFF
+  #Implicit Radiation:         OFF
+  #Cosmic Ray Transport:       OFF
+  #Cosmic Ray Diffusion:       OFF
+  #Self-Gravity:               Multigrid
+  #Super-Time-Stepping:        OFF
+  #Floating-point precision:   double
+  #Number of ghost cells:      4
+  #MPI parallelism:            OFF
+  #OpenMP parallelism:         OFF
+  #FFT:                        OFF
+  #HDF5 output:                OFF
+  #Compiler:                   g++
+  #Compilation command:        g++ -O3 -std=c++11
