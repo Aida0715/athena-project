35ae1a3ae3dee3c50f81dc991cbfae89e3d0c0d5
aian <k0545421@kadai.jp>
2026-04-01 19:33:24 +0900

    Toyouchi-test7.sh追加

---
+++ run/Toyouchi-test7.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# commit: 23d5653 //本シミュレーションに対応するjeans.cppをgitの履歴から追跡可
+# VΦを追加
+
+cd "$(dirname "$0")/.."
+
+OUTDIR=results/Toyouchi-test7
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+# 実行（ファイル名はinputに任せる）
+# 環境作成中のテスト計算用入力ファイルはToyouchi_testディレクトリに格納
+../../bin/athena -i ../../inputs/hydro/Toyouchi_test/athinput.Toyouchi_2
+
+# configure
+  #Problem generator:          jeans
+  #Coordinate system:          cartesian
+  #Equation of state:          isothermal
+  #Riemann solver:             hlle
+  #Magnetic fields:            OFF
+  #Relativistic dynamics:      OFF 
+  #General relativity:         OFF 
+  #Radiative Transfer:         OFF
+  #Implicit Radiation:         OFF
+  #Cosmic Ray Transport:       OFF
+  #Cosmic Ray Diffusion:       OFF
+  #Self-Gravity:               Multigrid
+  #Super-Time-Stepping:        OFF
+  #Floating-point precision:   double
+  #Number of ghost cells:      4
+  #MPI parallelism:            OFF
+  #OpenMP parallelism:         OFF
+  #FFT:                        OFF
+  #HDF5 output:                OFF
+  #Compiler:                   g++
+  #Compilation command:        g++ -O3 -std=c++11
