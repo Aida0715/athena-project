75765f24f00fc03646459d34f3ad862f7ec2f8f1
aian <k0545421@kadai.jp>
2026-04-03 21:14:44 +0900

    初期条件の密度フロアを削除

---
+++ run/Toyouchi-test10.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# commit: 9fbd0fd  //本シミュレーションに対応するjeans.cppをgitの履歴から追跡可
+# Toyouchi-test9でコアダンプしたので、密度フロアを削除した
+
+cd "$(dirname "$0")/.."
+
+OUTDIR=results/Toyouchi-test10
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+# 実行（ファイル名はinputに任せる）
+# 環境作成中のテスト計算用入力ファイルはToyouchi_testディレクトリに格納
+../../bin/athena -i ../../inputs/hydro/Toyouchi_test/athinput.Toyouchi_3
+
+# configure
+  #Problem generator:          jeans
+  #Coordinate system:          cartesian
+  #Equation of state:          isothermal
+  #Riemann solver:             hlle
+  #Magnetic fields:            OFF
+  #Relativistic dynamics:      OFF 
+  #General relativity:         OFF 
+  #Radiative Transfer:         OFF
+  #Implicit Radiation:         OFF
+  #Cosmic Ray Transport:       OFF
+  #Cosmic Ray Diffusion:       OFF
+  #Self-Gravity:               Multigrid
+  #Super-Time-Stepping:        OFF
+  #Floating-point precision:   double
+  #Number of ghost cells:      4
+  #MPI parallelism:            OFF
+  #OpenMP parallelism:         OFF
+  #FFT:                        OFF
+  #HDF5 output:                OFF
+  #Compiler:                   g++
+  #Compilation command:        g++ -O3 -std=c++11
