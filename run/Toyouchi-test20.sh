dc690784f0e35025629ef32be8965ec6b9b38790
aian <k0545421@kadai.jp>
2026-06-20 17:53:09 +0900

    AMRはJeansリファインのみON

---
+++ run/Toyouchi-test20.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# commit: a2f95b5  //本シミュレーションに対応するjeans.cppをgitの履歴から追跡可
+# AMRをJeansリファインのみON
+# 回転(vr,vphi)、中心星重力、自己重力はOFF
+
+cd "$(dirname "$0")/.."
+
+OUTDIR=results/Toyouchi-test20
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+# 実行（ファイル名はinputに任せる）
+# 環境作成中のテスト計算用入力ファイルはToyouchi_testディレクトリに格納
+../../bin/athena -i ../../inputs/hydro/Toyouchi_test/athinput.Toyouchi_12
+
+# configure
+ # Problem generator:            Toyouchi
+ #Coordinate system:            cartesian
+ #Equation of state:            isothermal
+ #Riemann solver:               hlle
+ #Magnetic fields:              OFF
+ #Number of scalars:            0
+ #Number of chemical species:   0
+ #Special relativity:           OFF
+ #General relativity:           OFF
+ #Radiative Transfer:           OFF
+ #Implicit Radiation:           OFF
+ #Cosmic Ray Transport:         OFF
+ #Cosmic Ray Diffusion:         OFF
+ #Frame transformations:        OFF
+ #Self-Gravity:                 OFF
+ #Super-Time-Stepping:          OFF
+ #Chemistry:                    OFF
+ #KIDA rates:                   OFF
+ #ChemRadiation:                OFF
+ #chem_ode_solver:              OFF
+ #Debug flags:                  OFF
+ #Code coverage flags:          OFF
+ #Linker flags:                  
+ #Floating-point precision:     double
+ #Number of ghost cells:        4
+ #MPI parallelism:              OFF
+ #OpenMP parallelism:           OFF
+ #FFT:                          OFF
+ #HDF5 output:                  OFF
+ #Compiler:                     g++
+ #Compilation command:          g++  -O3 -std=c++11
+
