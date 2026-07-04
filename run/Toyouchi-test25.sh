a3cf684107a3ff443257b323d5348e3535dfc0bb
aian <k0545421@kadai.jp>
2026-07-04 17:40:10 +0900

    use_nfw_gravityを追加。NFWをONにして中心星重力,自己重力OFF

---
+++ run/Toyouchi-test25.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# commit: 7e21893  //本シミュレーションに対応するToyouchi.cppをgitの履歴から追跡可
+# NFW追加
+# 自己重力、中心星重力、 回転(vr,vphi)はOFF
+
+cd "$(dirname "$0")/.."
+
+OUTDIR=results/Toyouchi-test25
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+# 実行（ファイル名はinputに任せる）
+# 環境作成中のテスト計算用入力ファイルはToyouchi_testディレクトリに格納
+../../bin/athena -i ../../inputs/hydro/Toyouchi_test/athinput.Toyouchi_17
+
+  #Problem generator:            Toyouchi
+  #Coordinate system:            cartesian
+  #Equation of state:            isothermal
+  #Riemann solver:               hlle
+  #Magnetic fields:              OFF
+  #Number of scalars:            0
+  #Number of chemical species:   0
+  #Special relativity:           OFF
+  #General relativity:           OFF
+  #Radiative Transfer:           OFF
+  #Implicit Radiation:           OFF
+  #Cosmic Ray Transport:         OFF
+  #Cosmic Ray Diffusion:         OFF
+  #Frame transformations:        OFF
+  #Self-Gravity:                 OFF
+  #Super-Time-Stepping:          OFF
+  #Chemistry:                    OFF
+  #KIDA rates:                   OFF
+  #ChemRadiation:                OFF
+  #chem_ode_solver:              OFF
+  #Debug flags:                  OFF
+  #Code coverage flags:          OFF
+  #Linker flags:                  
+  #Floating-point precision:     double
+  #Number of ghost cells:        4
+  #MPI parallelism:              OFF
+  #OpenMP parallelism:           OFF
+  #FFT:                          OFF
+  #HDF5 output:                  OFF
+  #Compiler:                     g++
+  #Compilation command:          g++  -O3 -std=c++11
+
+
